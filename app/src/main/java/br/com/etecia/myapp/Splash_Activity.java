82c43e94bd80a24f91f3eddc44d3b163e345142f
LBorgess <lucas_rancer@hotmail.com>
2023-05-11 21:12:22 -0300

    Execução

--- app/src/main/java/br/com/etecia/myapp/Splash_Activity.java
+++ app/src/main/java/br/com/etecia/myapp/Splash_Activity.java
@@ -2,7 +2,9 @@ package br.com.etecia.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class Splash_Activity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class Splash_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_layout);
+
+        // Criando o carregamento da janela de login
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Abrindo próxima janela
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                finish(); // Fecha janela
+            }
+        }, 3000);
+
     }
 }

ea8f0830bc9350ddb01c1b0ad829983bd4ffc30d
LBorgess <lucas_rancer@hotmail.com>
2023-05-18 21:32:16 -0300

    switch case do menu

--- app/src/main/java/br/com/etecia/myapp/MenuActivity.java
+++ app/src/main/java/br/com/etecia/myapp/MenuActivity.java
@@ -2,12 +2,13 @@ package br.com.etecia.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import com.google.android.material.card.MaterialCardView;
 
-public class MenuActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity implements View.OnClickListener {
 
     MaterialCardView idMenuPets, idMenuAlimentos,
             idMenuDoacao, idMenuServicos,
@@ -25,6 +26,37 @@ public class MenuActivity extends AppCompatActivity {
         idMenuLocalizar = findViewById(R.id.IdMenuLocalizar);
         idMenuShop = findViewById(R.id.IdMenuShop);
 
+        idMenuPets.setOnClickListener(this);
+        idMenuAlimentos.setOnClickListener(this);
+        idMenuDoacao.setOnClickListener(this);
+        idMenuServicos.setOnClickListener(this);
+        idMenuLocalizar.setOnClickListener(this);
+        idMenuShop.setOnClickListener(this);
 
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.IdMenuPets:
+                startActivity(new Intent(getApplicationContext(), PetsActivity.class));
+                break;
+            case R.id.IdMenuAlimentos:
+                startActivity(new Intent(getApplicationContext(), AlimentosActivity.class));
+                break;
+            case R.id.IdMenuDoacao:
+                startActivity(new Intent(getApplicationContext(), DoacaoActivity.class));
+                break;
+            case R.id.IdMenuServicos:
+                startActivity(new Intent(getApplicationContext(), ServicosActivity.class));
+                break;
+            case R.id.IdMenuLocalizar:
+                startActivity(new Intent(getApplicationContext(), LocalizarActivity.class));
+                break;
+            case R.id.IdMenuShop:
+                startActivity(new Intent(getApplicationContext(), ShopActivity.class));
+                break;
+        }
     }
 }

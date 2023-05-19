1306f79768a21358a8a51aded29c383ccf95b2f8
LBorgess <lucas_rancer@hotmail.com>
2023-05-18 21:23:43 -0300

    switch case java

--- app/src/main/java/br/com/etecia/myapp/MenuActivity.java
+++ app/src/main/java/br/com/etecia/myapp/MenuActivity.java
@@ -3,12 +3,28 @@ package br.com.etecia.myapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.card.MaterialCardView;
 
 public class MenuActivity extends AppCompatActivity {
+
+    MaterialCardView idMenuPets, idMenuAlimentos,
+            idMenuDoacao, idMenuServicos,
+            idMenuLocalizar, idMenuShop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_layout);
+
+        idMenuPets = findViewById(R.id.IdMenuPets);
+        idMenuAlimentos = findViewById(R.id.IdMenuAlimentos);
+        idMenuDoacao = findViewById(R.id.IdMenuDoacao);
+        idMenuServicos = findViewById(R.id.IdMenuServicos);
+        idMenuLocalizar = findViewById(R.id.IdMenuLocalizar);
+        idMenuShop = findViewById(R.id.IdMenuShop);
+
+
     }
 }

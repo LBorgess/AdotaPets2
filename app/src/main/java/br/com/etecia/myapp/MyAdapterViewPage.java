316a2b33d2a0b7127adc8dd89fe6c3ab364010e5
LBorgess <lucas_rancer@hotmail.com>
2023-05-25 21:23:42 -0300

    Fragments, Adapters

---
+++ app/src/main/java/br/com/etecia/myapp/MyAdapterViewPage.java
@@ -0,0 +1,25 @@
+package br.com.etecia.myapp;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+public class MyAdapterViewPage extends FragmentStatePagerAdapter {
+
+
+    public MyAdapterViewPage(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
